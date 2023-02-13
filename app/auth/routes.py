3cd164ff96b38fb12612be017f70d71750f829e4
Denis <denis.bargan2006@gmail.com>
2023-02-13 20:08:47 +0200

    Work with forms

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,14 +1,29 @@
 from . import bp
-from flask import render_template
-from app.extensions import db
-from app.models.user import Users
+from flask import render_template, request, redirect, url_for
 from .forms import LoginForm, RegisterForm
+# from app.extensions import db
+# from app.models.user import Users
+
 
 @bp.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('auth/login.html', form=LoginForm())
+    form = LoginForm()
+    users = {'denis':'12345', 'alex':'54321'}
+    
+    if form.validate_on_submit():
+        username = form.username.data
+        if username in users and users[username] == form.password.data:
+            return redirect(url_for('main.index'))  
+        
+        # or
+        # username = request.form.get('username')
+        # if username in users and users[username] == request.form.get('password'):
+        #     return redirect(url_for('main.index'))
+        
+    return render_template('auth/login.html', form=form)
 
 
 @bp.route('/register', methods=['POST', 'GET'])
 def register():
-    return render_template('auth/register.html', form=RegisterForm())
+    form = RegisterForm()
+    return render_template('auth/register.html', form=form)
