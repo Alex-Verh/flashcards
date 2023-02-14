05892c938af9ac7693dd8d1939bd92e5da68468b
Alexandru Verhovetchi <74010714+Alex-Verh@users.noreply.github.com>
2023-02-14 10:29:10 +0100

    Added username regex validation and fixed some css

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Regexp
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username:', validators=[DataRequired("Please enter your username"), Length(3, 15, "Length must be between 3-15 characters.")])
-    password = PasswordField('Password:', validators=[DataRequired(), Length(8, 20, "Length must be between 8-20 characters.")])
+    username = StringField('Username:', validators=[DataRequired("Please enter your username"), Regexp('^\w+$', message="Username must contain only letters or numbers"), Length(3, 15, "Length must be between 3-15 characters")])
+    password = PasswordField('Password:', validators=[DataRequired(), Length(8, 20, "Length must be between 8-20 characters")])
     submit = SubmitField('Login')
 
     
 class RegisterForm(FlaskForm):
-    name = StringField('Name:', validators=[DataRequired("Please enter your name"),  Length(3, 30,"Length must be between 3-30 characters.")])
-    username = StringField('Username:', validators=[DataRequired("Please enter your username"),  Length(3, 15,"Length must be between 3-15 characters.")])
-    password = PasswordField('Password:', validators=[DataRequired("Please enter your password"), Length(8, 20,"Length must be between 8-20 characters.")])
-    password1 = PasswordField('Repeat password:', validators=[DataRequired("Please repeat your password"), EqualTo('password'), Length(8, 20,"Length must be between 8-20 characters.")])
+    name = StringField('Name:', validators=[DataRequired("Please enter your name"), Regexp('^\w+$', message="Username must contain only letters or numbers"),  Length(3, 30,"Length must be between 3-30 characters")])
+    username = StringField('Username:', validators=[DataRequired("Please enter your username"), Regexp('^\w+$`', message="Username must contain only letters or numbers"),  Length(3, 15,"Length must be between 3-15 characters")])
+    password = PasswordField('Password:', validators=[DataRequired("Please enter your password"), Length(8, 20,"Length must be between 8-20 characters")])
+    password1 = PasswordField('Repeat password:', validators=[DataRequired("Please repeat your password"), EqualTo('password', "Passwords must match"), Length(8, 20,"Length must be between 8-20 characters.")])
     submit = SubmitField('Register')
