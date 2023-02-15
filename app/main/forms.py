b7a73087689f3904f34ca58ac0a01aaeb189d9b1
Denis <denis.bargan2006@gmail.com>
2023-02-15 21:34:53 +0200

    Added email validation

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Email, Length
 
 class ContactForm(FlaskForm):
     name = StringField('Enter Name:', validators=[DataRequired("Please enter your name"), Length(3, 15, "Length must be between 3-15 characters.")])
-    email = StringField('Email Address:', validators=[Email("Please enter your email address"), Length(3, 20, "Length must be between 3-20 characters.")])
+    email = StringField('Email Address:', validators=[DataRequired("Please enter your email address"), Email("Enter valid email"), Length(3, 20, "Length must be between 3-20 characters.")])
     title = StringField('Short Subject:', validators=[DataRequired("Please enter your title"), Length(3, 20, "Length must be between 3-20 characters.")])
     message = StringField('Message to Us:', validators=[DataRequired("Please enter your message"),  Length(3, 100, "Length must be between 3-100 characters.")])
     submit = SubmitField('Send to Us')
