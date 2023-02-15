f6e598bc4c6dbfad527f013c882fd63fc61f662e
Denis <denis.bargan2006@gmail.com>
2023-02-15 22:23:42 +0200

    Restructured

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Length, EqualTo, Regexp
+from wtforms.validators import DataRequired, Length, EqualTo, Regexp, ValidationError
 
 
 class LoginForm(FlaskForm):
