0b614a043334be0456ec67a82bf74c9ad95fc28e
Alexandru Verhovetchi <74010714+Alex-Verh@users.noreply.github.com>
2023-02-20 20:22:38 +0100

    Fixed search bar

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,14 +1,14 @@
 from . import bp
-from flask import render_template
-from flask_login import login_required, current_user
-from app.models import FlashCard
+from flask import render_template, request
 
 @bp.route('/')
 def index():
     return render_template('main/index.html')
 
 
-@bp.route('/home')
+@bp.route('/home', methods = ['GET'])
 def home():
+    search = request.args.get('search')
+    if search:
+        print("TY ebobo!", search)
     return render_template('main/home.html')
-
