ee37d5b4ed4ffdb0840f37ba52ab6bdd43799fbf
Denis <denis.bargan2006@gmail.com>
2023-02-20 21:23:44 +0200

    Added sqlachemy models

--- app/models.py
+++ app/models.py
@@ -3,18 +3,48 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=True)
-    username = db.Column(db.String(50), unique=True, nullable=True)
-    password = db.Column(db.String(500), nullable=True)
+    name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(500), nullable=False)
+    card_sets = db.relationship('CardSet', backref='user', lazy=True)
 
     def __repr__(self):
         return f'<User "{self.username}">'
     
+class CardSet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    cards = db.relationship('FlashCard', backref='cardset', lazy=True)
+    
+    def __repr__(self):
+        return f"<CardSet: {self.name}>"
+    
 class FlashCard(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(40), nullable=True)
-    content = db.Column(db.String(300), nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    title = db.Column(db.String(40), nullable=False)
+    content = db.Column(db.String(300), nullable=False)
+    cardset_id = db.Column(db.Integer, db.ForeignKey('cardset.id'))
+    images = db.relationship('CardImage', backref='flashcard', lazy=True)
+    audio = db.relationship('CardAudio', backref='flashcard', lazy=True)
 
     def __repr__(self):
         return f"<FlashCard: {self.title}>"
+    
+class CardImage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.Blob)
+    side = db.Column(db.Integer, nullable=False)
+    card_id = db.Column(db.Integer, db.ForeignKey('flashcard.id'))
+    
+    def __repr__(self):
+        return f"<CardImage: {self.id}>"
+    
+class CardAudio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    audio = db.Column(db.Blob)
+    side = db.Column(db.Integer, nullable=False)
+    card_id = db.Column(db.Integer, db.ForeignKey('flashcard.id'))
+    
+    def __repr__(self):
+        return f"<CardAudio: {self.id}>"    
