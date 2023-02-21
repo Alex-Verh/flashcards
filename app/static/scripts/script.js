1b9b52bc7298388ef332f77578f0c8fb8ef76096
Alexandru Verhovetchi <74010714+Alex-Verh@users.noreply.github.com>
2023-02-21 21:08:09 +0100

    Fixed css for profile

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -10,3 +10,11 @@ document.body.onpointermove = event => {
     }, { duration : 3000, fill: "forwards" });
 }
 
+function deleteConf() {
+    let text = "Are you sure you want to delete this set?";
+    if (confirm(text) == true) {
+        text = "You have succesfully deleted it!";
+      } else {
+        text = "You canceled!";
+      }
+}
