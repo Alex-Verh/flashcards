380588a2945fe8767d243b2006c16c3afccfb7fc
Denis <denis.bargan2006@gmail.com>
2023-02-23 18:01:48 +0200

    Added cotegory model

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from .extensions import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -21,6 +22,8 @@ class CardSet(db.Model):
     __tablename__ = 'card_sets'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
+    is_public = db.Column(db.Boolean, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('card_set_categories.id', ondelete="SET DEFAULT"), default=21)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="CASCADE"), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), default=func.now())
     
@@ -51,3 +54,13 @@ class CardSetSave(db.Model):
     
     def __repr__(self):
         return f"<CardSetSave: {self.cardset_id} - {self.user_id}>" 
+    
+
+class CardSetCategory(db.Model):
+    __tablename__ = 'card_set_categories'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(25), nullable=False)
+    
+    def __repr__(self):
+        return f"<CardSetCategory: {self.title}>"
+    
