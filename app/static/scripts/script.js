3e132580e38a8959fc09ea29c3edc809368450f8
Alexandru Verhovetchi <74010714+Alex-Verh@users.noreply.github.com>
2023-02-24 18:38:45 +0100

    Scroll sensitivity

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -34,3 +34,26 @@ $('.dropdown .dropdown-menu li').click(function () {
   $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
 });
 /*End Dropdown Menu*/
+
+//Scroll sensitivy slowdown
+function wheel(event) {
+  var delta = 0;
+  if (event.wheelDelta) {(delta = event.wheelDelta / 120);}
+  else if (event.detail) {(delta = -event.detail / 3);}
+
+  handle(delta);
+  if (event.preventDefault) {(event.preventDefault());}
+  event.returnValue = false;
+}
+
+function handle(delta) {
+  var time = 1000;
+  var distance = 300;
+
+  $('html, body').stop().animate({
+      scrollTop: $(window).scrollTop() - (distance * delta)
+  }, time );
+}
+
+if (window.addEventListener) {window.addEventListener('DOMMouseScroll', wheel, false);}
+window.onmousewheel = document.onmousewheel = wheel;
