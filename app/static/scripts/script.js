a2e3c6b39306a6f850e6c269fedaeb7825c4d8b1
Alex-Verh <alexandrverhovetchi@gmail.com>
2023-03-01 21:12:53 +0200

    Fixed css, added return back button, fixed buttons

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -35,30 +35,6 @@ $('.dropdown .dropdown-menu li').click(function () {
 });
 /*End Dropdown Menu*/
 
-//Scroll sensitivy slowdown
-function wheel(event) {
-  var delta = 0;
-  if (event.wheelDelta) {(delta = event.wheelDelta / 120);}
-  else if (event.detail) {(delta = -event.detail / 3);}
-
-  handle(delta);
-  if (event.preventDefault) {(event.preventDefault());}
-  event.returnValue = false;
-}
-
-function handle(delta) {
-  var time = 1000;
-  var distance = 300;
-
-  $('html, body').stop().animate({
-      scrollTop: $(window).scrollTop() - (distance * delta)
-  }, time );
-}
-
-if (window.addEventListener) {window.addEventListener('DOMMouseScroll', wheel, false);}
-window.onmousewheel = document.onmousewheel = wheel;
-//end scroll
-
 //  Category of new set 
   const dropdown = document.querySelector('.dropdown');
   const input = dropdown.querySelector('input[name="category"]');
