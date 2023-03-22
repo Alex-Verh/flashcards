cd8aed12475cee37a4fbd336c10e9eab7b0a10fa
Alexandru Verhovetchi <74010714+Alex-Verh@users.noreply.github.com>
2023-03-22 13:27:43 +0100

    Added buttons in edit profile

--- app/static/scripts/editProfile.js
+++ app/static/scripts/editProfile.js
@@ -1,16 +1,17 @@
 function edit_profile() {
     const edit_modal = document.getElementById("edit_profile_modal");
 
+    try {
+        document.getElementById("edit-profile").addEventListener('click', function(event) {
+            event.preventDefault();
+            edit_modal.classList.add('transition')
+        });
 
-    document.getElementById("edit-profile").addEventListener('click', function(event) {
-        event.preventDefault();
-        edit_modal.classList.add('transition')
-    });
-
-    edit_modal.querySelector('#edit-close').addEventListener('click', function(event) {
-        event.preventDefault();
-        edit_modal.classList.remove('transition')
-    });
+        edit_modal.querySelector('#edit-close').addEventListener('click', function(event) {
+            event.preventDefault();
+            edit_modal.classList.remove('transition')
+        });
+    } catch (e) {console.log(e);}
 }
 
 export {edit_profile}
