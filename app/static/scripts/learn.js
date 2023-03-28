3fb140eca8281ac935a69b6dd75df8108c4d4a57
Alexandru Verhovetchi <74010714+Alex-Verh@users.noreply.github.com>
2023-03-28 23:19:04 +0200

    Added some basic JS validation

--- app/static/scripts/learn.js
+++ app/static/scripts/learn.js
@@ -16,20 +16,38 @@ function chooseLearn() {
 
 function learn_modal() {
     const learn_modal = document.getElementById("learn_modal");
+    const learn_title = document.getElementById("learn-title");
+
 
     document
       .getElementById("start_learning")
       .addEventListener("click", function (event) {
         event.preventDefault();
-        learn_modal.classList.add("transition");
+        if (document.querySelector(".selected-set")) {
+          learn_modal.classList.add("transition");
+
+          // Checks what mode is checked
+          if(document.querySelector('input[name="mode"]:checked').value == 1) {
+            learn_title.innerHTML = "Guess content:"
+          } else {
+              learn_title.innerHTML = "Guess title:"
+          }
+
+        } else {
+          alert("Choose a set to start learning!")
+        }
       });
   
     learn_modal
       .querySelector("#learn-close")
       .addEventListener("click", function (event) {
         event.preventDefault();
-        learn_modal.classList.remove("transition");
+        const userConfirm = confirm('Are you sure you want to quit learning process?')
+        if (userConfirm) {
+          learn_modal.classList.remove("transition");
+        }
       });
+
 }
 
 export { chooseLearn, learn_modal };
