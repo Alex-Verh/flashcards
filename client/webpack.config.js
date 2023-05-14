41ba61d9896ac1ca66d739c4c9a8fb45db044714
Denis <denis.bargan2006@gmail.com>
2023-05-14 20:52:23 +0300

    Added bundling modes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,77 +4,101 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
-const htmlPlugin = (name) =>
-  new HtmlWebpackPlugin({
-    template: `./templates/${name}.html`,
-    filename: `${name}.html`,
-    chunks: [name],
-    minify: {
-      collapseWhitespace: true,
-      removeComments: true,
-      removeRedundantAttributes: true,
-      removeStyleLinkTypeAttributes: true,
-      removeScriptTypeAttributes: true,
-      removeAttributeQuotes: true,
-      useShortDoctype: true,
-    },
-  });
+pages = ["main"];
 
-module.exports = (env, argv) => ({
-  context: path.resolve(__dirname, "src"),
-  mode: "development",
-  entry: {
-    main: "./js/main.js",
-    // cards: "./cards.js",
-    // cardsets: "./cardsets.js",
-    // login: "./login.js",
-    // register: "./register.js",
-  },
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  devServer: {
-    static: path.resolve(__dirname, "dist"),
-  },
-  optimization: {
-    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.s[ac]ss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "img/[name].[hash][ext]",
-        },
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "fonts/[name].[hash][ext]",
-        },
-      },
-    ],
-  },
-  plugins: [
-    htmlPlugin("main"),
-    // htmlPlugin("cards"),
-    // htmlPlugin("cardsets"),
-    // htmlPlugin("login"),
-    // htmlPlugin("register"),
-    new MiniCssExtractPlugin({
-      filename: "css/[name].css",
+const filename = (ext = "[ext]") =>
+  mode === "production" ? `[name].[hash].${ext}` : `[name].${ext}`;
+
+const entry = () =>
+  pages.reduce(
+    (config, page) => ({
+      ...config,
+      [page]: `./js/${page}.js`,
     }),
-  ],
+    {}
+  );
+
+const output = () => ({
+  filename: filename("js"),
+  path: path.resolve(__dirname, "dist"),
+  clean: true,
 });
+
+const optimization = () => {
+  const optimizationConfig = {
+    splitChunks: {
+      chunks: "all",
+    },
+  };
+  if (mode === "production")
+    optimizationConfig.minimizer = [
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
+    ];
+};
+
+const devServer = () => ({
+  static: path.resolve(__dirname, "dist"),
+});
+
+const devtool = () => (mode === "production" ? "source-map" : "eval");
+
+const rules = () => [
+  {
+    test: /\.css$/i,
+    use: [MiniCssExtractPlugin.loader, "css-loader"],
+  },
+  {
+    test: /\.s[ac]ss$/i,
+    use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+  },
+  {
+    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+    type: "asset/resource",
+    generator: {
+      filename: "img/" + filename(),
+    },
+  },
+  {
+    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+    type: "asset/resource",
+    generator: {
+      filename: "fonts/" + filename(),
+    },
+  },
+];
+
+const htmlPlugins = () =>
+  pages.map(
+    (page) =>
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: `./pages/${page}.html`,
+        filename: `${page}.html`,
+        chunks: [page],
+      })
+  );
+
+const plugins = () => [
+  ...htmlPlugins(),
+  new MiniCssExtractPlugin({
+    filename: "css/" + filename("css"),
+  }),
+];
+
+module.exports = (env) => {
+  global.mode = env.MODE;
+  return {
+    context: path.resolve(__dirname, "src"),
+    mode: mode,
+    entry: entry(),
+    output: output(),
+    optimization: optimization(),
+    devServer: devServer(),
+    devtool: devtool(),
+    module: {
+      rules: rules(),
+    },
+    plugins: plugins(),
+  };
+};
