b2c1784d29da01aacd271783d79ff3fcd7e98a41
Denis <denis.bargan2006@gmail.com>
2023-05-14 10:40:10 +0300

    Added webpack

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ up:
 	docker-compose up -d
 down:
 	docker-compose down && docker network prune --force
+init migrations:
+	cd server && flask --app app db init && cd ..
 migrate:
 	cd server && flask --app app db migrate && flask --app app db upgrade && cd ..
 run:
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,63 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+const htmlPlugin = (name) =>
+  new HtmlWebpackPlugin({
+    template: `./src/${name}.html`,
+    filename: `${name}.html`,
+    chunks: [name],
+    minify: {
+      collapseWhitespace: true,
+      removeComments: true,
+      removeRedundantAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeAttributeQuotes: true,
+      useShortDoctype: true,
+    },
+  });
+
+module.exports = (env, argv) => ({
+  context: path.resolve(__dirname, "src"),
+  entry: {
+    main: "./main.js",
+    cards: "./cards.js",
+    cardsets: "./cardsets.js",
+    login: "./login.js",
+    register: "./register.js",
   },
-};
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    htmlPlugin("main"),
+    htmlPlugin("cards"),
+    htmlPlugin("cardsets"),
+    htmlPlugin("login"),
+    htmlPlugin("register"),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash].css",
+    }),
+  ],
+});
