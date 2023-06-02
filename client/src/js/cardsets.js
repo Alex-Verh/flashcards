1bfe7313057e76ace24abb407ad1abb4e6c530a9
denis <denis.bargan2006@gmail.com>
2023-06-02 17:08:13 +0300

    added dropdowns functionality

--- client/src/js/cardsets.js
+++ client/src/js/cardsets.js
@@ -1,10 +1,5 @@
 import "../sass/pages/cardsets.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const dropdowns = document.querySelectorAll(".dropdown__btn");
-  dropdowns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      btn.parentElement.focus();
-    });
-  });
+  import("./modules/dropdown");
 });
