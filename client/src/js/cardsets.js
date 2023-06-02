9d4a7d470a3c6b8c13cd3fd4884df50e028abcca
denis <denis.bargan2006@gmail.com>
2023-06-02 12:01:43 +0300

    Made dropdowns

--- client/src/js/cardsets.js
+++ client/src/js/cardsets.js
@@ -1,1 +1,10 @@
 import "../sass/pages/cardsets.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const dropdowns = document.querySelectorAll(".dropdown__btn");
+  dropdowns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btn.parentElement.focus();
+    });
+  });
+});
--- client/src/js/main.js
+++ client/src/js/main.js
@@ -1,1 +1,9 @@
 import "../sass/pages/main.scss";
+document.addEventListener("DOMContentLoaded", () => {
+  const dropdowns = document.querySelectorAll(".dropdown__btn");
+  dropdowns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btn.parentElement.focus();
+    });
+  });
+});
