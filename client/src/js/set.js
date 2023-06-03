1c1e7d437ae5a78e5428f73332c7d6e0aa749483
Alex-Verh <alexandrverhovetchi@gmail.com>
2023-06-03 12:29:01 +0200

    Added flashcard js rotation

--- client/src/js/set.js
+++ client/src/js/set.js
@@ -1,1 +1,10 @@
 import "../sass/pages/set.scss";
+
+const flashcards = document.querySelector(".flashcards");
+
+flashcards.addEventListener("click", (event) => {
+    const flashcard = event.target.closest(".flashcards__card");
+    if (flashcard) {
+        flashcard.classList.toggle("flashcards__card-active");
+    } 
+})
