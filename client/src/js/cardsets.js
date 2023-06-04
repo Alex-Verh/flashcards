4c7668201440c1edf9411379f1ff8397c986e21f
Alex-Verh <alexandrverhovetchi@gmail.com>
2023-06-04 10:13:17 +0200

    Added profile dropdown opening

--- client/src/js/cardsets.js
+++ client/src/js/cardsets.js
@@ -2,4 +2,13 @@ import "../sass/pages/cardsets.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
   import("./modules/dropdown");
+
+  const profileButton = document.querySelector(".nav__profile")
+  const profileModal = document.querySelector(".profile-modal")
+
+  if (profileButton) {
+    profileButton.addEventListener("click", () => {
+      profileModal.classList.toggle("none");
+    })
+  }
 });
